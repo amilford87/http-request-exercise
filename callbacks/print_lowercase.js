64885518d9d104838e0a2f8ba30acfb31bb9b474
amilford87 <amilford87@gmail.com>
2019-02-11 18:23:30 +0000

    adding folder

---
+++ callbacks/print_lowercase.js
@@ -0,0 +1,15 @@
+var getHTML = require('./http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/lowercase.html'
+};
+
+function printUpperCase (html) {
+    var bufferedChunks = "";
+    bufferedChunks += html;
+    var upperCaseText = bufferedChunks.toLowerCase();
+    console.log(upperCaseText);
+}
+
+getHTML(requestOptions, printUpperCase);
---
+++ callbacks/print_uppercase.js
@@ -0,0 +1,15 @@
+var getHTML = require('./http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function printUpperCase (html) {
+    var bufferedChunks = "";
+    bufferedChunks += html;
+    var upperCaseText = bufferedChunks.toUpperCase();
+    console.log(upperCaseText);
+}
+
+getHTML(requestOptions, printUpperCase);
