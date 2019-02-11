e6ae582e5d9cd78f3dd4140064f3d60255a84ade
amilford87 <amilford87@gmail.com>
2019-02-11 16:30:45 +0000

    fixed step 1, working now

--- step1.js
+++ step1.js
@@ -9,9 +9,14 @@ function getAndPrintHTMLChunks () {
   
     https.get(requestOptions, function (response) {
         response.setEncoding('utf8');
-        response.on(data, function (data){
+        response.on('data', function (data){
             console.log(data.concat('\n'));
+        });
+        response.on('end', function(){
+            console.log('Response stream completed');
         });
     });
   
   }
+
+  console.log(getAndPrintHTMLChunks());
